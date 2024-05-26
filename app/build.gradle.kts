c9e6d1bc11d37b2780c3f50c5b2111355f4fd90c
ede73 <ede@iki.fi>
2024-05-25 21:08:11 -0700

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 34
 
-        val (versionMajor, versionMinor, versionPatch, versionBuild) = listOf(3, 0, 16, 0)
+        val (versionMajor, versionMinor, versionPatch, versionBuild) = listOf(3, 0, 17, 0)
         versionCode =
             versionMajor * 10000 + versionMinor * 1000 + versionPatch * 100 + versionBuild
         versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
