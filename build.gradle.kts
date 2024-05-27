18dedc17576d0f50d93fe36770b3613a391ca375
ede73 <ede@iki.fi>
2024-05-27 15:18:15 -0700

    enable PowerAssert

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
+    kotlin("plugin.power-assert") version "2.0.0"
+}
+
+// https://kotlinlang.org/docs/whatsnew20.html#experimental-kotlin-power-assert-compiler-plugin
+powerAssert {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    functions = listOf("kotlin.assert", "kotlin.test.assertTrue")
 }
