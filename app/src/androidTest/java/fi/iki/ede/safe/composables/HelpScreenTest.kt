2bb581d6e2112ccb457ddbda81ea9bb506d19db5
ede73 <ede@iki.fi>
2024-06-04 13:42:22 -0700

    help screen todo

--- app/src/androidTest/java/fi/iki/ede/safe/composables/HelpScreenTest.kt
+++ app/src/androidTest/java/fi/iki/ede/safe/composables/HelpScreenTest.kt
@@ -5,11 +5,13 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import fi.iki.ede.safe.ui.TestTag
 import fi.iki.ede.safe.ui.activities.HelpScreen
 import fi.iki.ede.safe.ui.onNodeWithTag
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
-@Ignore
+/**
+ * Test plan, verify that...
+ * - help displays the help [helpScreenOpens]
+ */
 class HelpScreenTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<HelpScreen>()
