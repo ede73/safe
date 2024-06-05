3abd07e03faa78cec8c75847f010b72ed7628e98
ede73 <ede@iki.fi>
2024-06-04 19:08:35 -0700

    one empty test required, else fails

--- app/src/androidTest/java/fi/iki/ede/safe/SiteEntryListScreenTest.kt
+++ app/src/androidTest/java/fi/iki/ede/safe/SiteEntryListScreenTest.kt
@@ -14,6 +14,7 @@ import org.junit.AfterClass
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 /**
@@ -35,6 +36,11 @@ class SiteEntryListScreenTest {
     @Before
     fun beforeEachTest() {
         MockDBHelper.initializeBasicTestDataModel()
+    }
+
+    @Test
+    fun empty() {
+
     }
 
     companion object {
