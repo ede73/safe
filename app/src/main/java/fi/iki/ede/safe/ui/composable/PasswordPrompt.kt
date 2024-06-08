5401cd1ac663f0cad3b56b11026e664387131bf7
ede73 <ede@iki.fi>
2024-06-08 12:21:59 -0700

    Enter master password preview

--- app/src/main/java/fi/iki/ede/safe/ui/composable/PasswordPrompt.kt
+++ app/src/main/java/fi/iki/ede/safe/ui/composable/PasswordPrompt.kt
@@ -10,11 +10,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import fi.iki.ede.crypto.Password
 import fi.iki.ede.safe.R
 import fi.iki.ede.safe.ui.TestTag
 import fi.iki.ede.safe.ui.testTag
 import fi.iki.ede.safe.ui.theme.SafeButton
+import fi.iki.ede.safe.ui.theme.SafeTheme
 
 @Composable
 fun PasswordPrompt(
@@ -43,5 +45,22 @@ fun PasswordPrompt(
             },
             modifier = Modifier.testTag(TestTag.TEST_TAG_LOGIN_BUTTON)
         ) { Text(stringResource(id = R.string.login_button)) }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PasswordPromptPreview() {
+    SafeTheme {
+        Column {
+            Text(text = "------ First time init")
+            PasswordPrompt(firstTimeInitialize = true) {
+                println("Got a good password")
+            }
+            Text(text = "------ Normal login")
+            PasswordPrompt(firstTimeInitialize = false) {
+                println("Got a good password")
+            }
+        }
     }
 }
