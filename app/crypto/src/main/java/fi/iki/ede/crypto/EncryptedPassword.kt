4f2d5b2ca96f61a90d5feb94e17c859ad570a283
ede73 <ede@iki.fi>
2024-06-13 12:06:42 -0700

    Remvoe hashcode/equals from Encrypted password, not needed

--- app/crypto/src/main/java/fi/iki/ede/crypto/EncryptedPassword.kt
+++ app/crypto/src/main/java/fi/iki/ede/crypto/EncryptedPassword.kt
@@ -11,10 +11,6 @@ import fi.iki.ede.crypto.support.DisallowedFunctions
  */
 data class EncryptedPassword(val ivCipherText: IVCipherText) : DisallowedFunctions() {
     fun isEmpty(): Boolean = ivCipherText.isEmpty()
-    override fun hashCode() = ivCipherText.hashCode()
-
-    override fun equals(other: Any?): Boolean =
-        (other != null) && (this.hashCode() == (other as? EncryptedPassword)?.hashCode())
 
     companion object {
         fun getEmpty() = EncryptedPassword(IVCipherText.getEmpty())
