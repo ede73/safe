04fd0e130be67853816537795ef4935ff41be715
ede73 <ede@iki.fi>
2024-06-16 12:01:10 -0700

    beautify entry lists

--- app/src/main/java/fi/iki/ede/safe/ui/composable/SiteEntryRowHeader.kt
+++ app/src/main/java/fi/iki/ede/safe/ui/composable/SiteEntryRowHeader.kt
@@ -1,15 +1,26 @@
 package fi.iki.ede.safe.ui.composable
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fi.iki.ede.crypto.IVCipherText
+import fi.iki.ede.crypto.keystore.KeyStoreHelperFactory
+import fi.iki.ede.safe.model.DecryptableCategoryEntry
+import fi.iki.ede.safe.model.DecryptableSiteEntry
 import fi.iki.ede.safe.ui.theme.LocalSafeTheme
 import fi.iki.ede.safe.ui.theme.SafeListItem
 import fi.iki.ede.safe.ui.theme.SafeTheme
+
+fun Color.darken(factor: Float): Color {
+    return copy(alpha = alpha * factor)
+}
 
 /**
  * Display nice big start letter of the given header string
@@ -20,22 +31,18 @@ fun SiteEntryRowHeader(headerString: String) {
     val safeTheme = LocalSafeTheme.current
 
     SafeListItem(
-        fillWidthFraction = 0.4f,
-        //modifier = Modifier.alpha(0.7f),
-        // TODO: really ugly :(
-        yOffset = (10).dp
+        fillWidthFraction = 0.2f,
+        yOffset = 32.dp,
+        color = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant.darken(0.8f),
+        )
     ) {
-        Row(
+        Text(
+            text = headerStart,
             modifier = Modifier
-                .padding(vertical = 6.dp)
-        ) {
-            Text(
-                text = headerStart,
-                modifier = Modifier
-                    .padding(14.dp),
-                style = safeTheme.customFonts.listHeaders
-            )
-        }
+                .padding(14.dp),
+            style = safeTheme.customFonts.listHeaders
+        )
     }
 }
 
@@ -43,6 +50,22 @@ fun SiteEntryRowHeader(headerString: String) {
 @Composable
 fun SiteEntryRowHeaderPreview() {
     SafeTheme {
-        SiteEntryRowHeader("A")
+        KeyStoreHelperFactory.encrypterProvider = { IVCipherText(it, it) }
+        KeyStoreHelperFactory.decrypterProvider = { it.cipherText }
+        val encrypter = KeyStoreHelperFactory.getEncrypter()
+        val site1 = DecryptableSiteEntry(1).apply {
+            description = encrypter("Description1".toByteArray())
+        }
+        val site2 = DecryptableSiteEntry(2).apply {
+            description = encrypter("Description1".toByteArray())
+        }
+        val cat = DecryptableCategoryEntry().apply {
+            encryptedName = encrypter("Category".toByteArray())
+        }
+        Column {
+            SiteEntryRowHeader("Q")
+            SiteEntryRow(site1, listOf(cat))
+            SiteEntryRow(site2, listOf(cat))
+        }
     }
 }
--- app/src/main/java/fi/iki/ede/safe/ui/theme/SafeListItem.kt
+++ app/src/main/java/fi/iki/ede/safe/ui/theme/SafeListItem.kt
@@ -1,9 +1,12 @@
 package fi.iki.ede.safe.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,6 +18,10 @@ fun SafeListItem(
     modifier: Modifier = Modifier,
     fillWidthFraction: Float = 1f,
     yOffset: Dp = 0.dp,
+    borderColor: BorderStroke? = null,
+    color: CardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+    ),
     content: @Composable () -> Unit
 ) {
     Card(
@@ -22,6 +29,8 @@ fun SafeListItem(
             .padding(6.dp)
             .offset(y = yOffset)
             .fillMaxWidth(fillWidthFraction),
+        colors = color,
+        border = borderColor,
         shape = MaterialTheme.shapes.medium
     ) {
         content()
