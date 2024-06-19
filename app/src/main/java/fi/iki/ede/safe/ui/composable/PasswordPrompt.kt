27680a800e228f3f0fd6a46b9fc1be6994657e0e
ede73 <ede@iki.fi>
2024-06-18 21:19:31 -0700

    wrong password animation

--- app/src/main/java/fi/iki/ede/safe/ui/composable/PasswordPrompt.kt
+++ app/src/main/java/fi/iki/ede/safe/ui/composable/PasswordPrompt.kt
@@ -1,16 +1,24 @@
 package fi.iki.ede.safe.ui.composable
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fi.iki.ede.crypto.Password
 import fi.iki.ede.safe.R
 import fi.iki.ede.safe.ui.TestTag
@@ -19,6 +27,7 @@ import fi.iki.ede.safe.ui.activities.LoginStyle
 import fi.iki.ede.safe.ui.testTag
 import fi.iki.ede.safe.ui.theme.SafeButton
 import fi.iki.ede.safe.ui.theme.SafeTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun PasswordPrompt(
@@ -28,7 +37,44 @@ fun PasswordPrompt(
 ) {
     var loginStyle by remember { mutableStateOf(loginPrecondition) }
     //var loginInProgress by remember { mutableStateOf(false) }
+    var wrongPassword by remember { mutableStateOf(false) }
     val passwordMinimumLength = integerResource(id = R.integer.password_minimum_length)
+
+    LaunchedEffect(wrongPassword) {
+        if (wrongPassword) {
+            delay(1000) // Duration of the effect
+            wrongPassword = false
+        }
+    }
+
+//    val color = animateColorAsState(
+//        targetValue = if (wrongPassword) Color.Red else Color.Transparent,
+//        animationSpec = infiniteRepeatable(
+//            animation = keyframes {
+//                durationMillis = 1000
+//                Color.Red at 500
+//                Color.Transparent at 1000
+//            },
+//            repeatMode = RepeatMode.Reverse
+//        ), label = "Wrong password"
+//    )
+//
+//    val shake = animateOffsetAsState(
+//        targetValue = if (wrongPassword) Offset(10f, 0f) else Offset.Zero,
+//        animationSpec = infiniteRepeatable(
+//            animation = tween(durationMillis = 50, easing = LinearEasing),
+//            repeatMode = RepeatMode.Reverse
+//        ), label = "Wrong password"
+//    )
+    val color = animateColorAsState(
+        targetValue = if (wrongPassword) Color.Red else Color.Transparent,
+        animationSpec = tween(durationMillis = 500)
+    )
+
+    val shake = animateDpAsState(
+        targetValue = if (wrongPassword) 10.dp else 0.dp,
+        animationSpec = tween(durationMillis = 50)
+    )
     Column {
         Text(text = stringResource(id = R.string.login_tip))
         var verifiedPassword: Password by remember { mutableStateOf(Password.getEmpty()) }
@@ -36,7 +82,13 @@ fun PasswordPrompt(
             loginStyle == LoginPrecondition.FIRST_TIME_LOGIN_EMPTY_DATABASE,
             R.string.login_password_tip,
             R.string.login_verify_password_tip,
-            modifier.testTag(TestTag.TEST_TAG_PASSWORD_PROMPT),
+            modifier
+                .testTag(TestTag.TEST_TAG_PASSWORD_PROMPT)
+                .border(width = 2.dp, color = color.value)
+                .graphicsLayer {
+                    translationX = shake.value.toPx()
+                    translationY = shake.value.toPx()
+                },
             onMatchingPasswords = {
                 verifiedPassword = it
             }
@@ -51,7 +103,7 @@ fun PasswordPrompt(
                     LoginPrecondition.FIRST_TIME_LOGIN_EMPTY_DATABASE -> LoginStyle.FIRST_TIME_LOGIN_CLEAR_DATABASE
                 }
                 if (!goodPasswordEntered(whatToDoAfterLogin, verifiedPassword)) {
-                    //loginInProgress = false
+                    wrongPassword = true
                 }
             },
             enabled = /*!loginInProgress &&*/ (if (loginStyle == LoginPrecondition.FIRST_TIME_LOGIN_EMPTY_DATABASE)
