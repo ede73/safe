99f79b4db084d7f14ec257b7df2f6ff277d9033e
ede73 <ede@iki.fi>
2024-06-25 00:58:34 -0700

    remove comment

--- app/src/main/java/fi/iki/ede/safe/model/DataModel.kt
+++ app/src/main/java/fi/iki/ede/safe/model/DataModel.kt
@@ -275,8 +275,6 @@ object DataModel {
     }
 
     suspend fun loadFromDatabase() {
-        // TODO:
-        // withContext(Dispatchers.IO) {???
         _categories.clear()
 
         fun loadCategoriesFromDB() {
