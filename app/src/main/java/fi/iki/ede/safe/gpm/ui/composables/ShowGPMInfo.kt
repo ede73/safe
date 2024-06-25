1192cf93956f2310af63a1dcc5dddbb7a9892683
ede73 <ede@iki.fi>
2024-06-24 22:10:30 -0700

    ShowGPMPreview

--- app/src/main/java/fi/iki/ede/safe/gpm/ui/composables/ShowGPMInfo.kt
+++ app/src/main/java/fi/iki/ede/safe/gpm/ui/composables/ShowGPMInfo.kt
@@ -1,8 +1,13 @@
 package fi.iki.ede.safe.gpm.ui.composables
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import fi.iki.ede.crypto.BuildConfig
+import fi.iki.ede.crypto.IVCipherText
+import fi.iki.ede.crypto.keystore.KeyStoreHelperFactory
+import fi.iki.ede.gpm.model.IncomingGPM
 import fi.iki.ede.gpm.model.SavedGPM
+import fi.iki.ede.safe.ui.theme.SafeTheme
 
 @Composable
 fun ShowGPMInfo(item: SavedGPM, onDismiss: () -> Unit) {
@@ -13,4 +18,17 @@ fun ShowGPMInfo(item: SavedGPM, onDismiss: () -> Unit) {
             } else it
         }
     UsageInfo(dump, onDismiss = onDismiss)
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ShowGPMInfoPreview() {
+    KeyStoreHelperFactory.encrypterProvider = { IVCipherText(it, it) }
+    KeyStoreHelperFactory.decrypterProvider = { it.cipherText }
+    val fakeSavedGPM =
+        SavedGPM(0, IncomingGPM.makeFromCSVImport("name", "http://acme", "user", "pwd", "note"))
+    SafeTheme {
+        ShowGPMInfo(fakeSavedGPM, {})
+    }
 }
