dd601f608c01f53499142c73172f6c8edbc6a67f
ede73 <ede@iki.fi>
2024-06-27 22:27:06 -0700

    yesno dialog

---
+++ app/src/main/java/fi/iki/ede/safe/ui/composable/YesNoDialog.kt
@@ -0,0 +1,42 @@
+package fi.iki.ede.safe.ui.composable
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import fi.iki.ede.safe.ui.theme.SafeButton
+
+@Composable
+fun YesNoDialog(
+    openDialog: MutableState<Boolean>,
+    title: String,
+    text: String? = null,
+    positiveText: String,
+    positive: () -> Unit,
+    negativeText: String? = null,
+    negative: () -> Unit = {},
+    dismissed: () -> Unit = {}
+) {
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = { dismissed() },
+            confirmButton = {
+                SafeButton(onClick = {
+                    positive()
+                    openDialog.value = false
+                }) { Text(positiveText) }
+            },
+            dismissButton = {
+                SafeButton(onClick = {
+                    negative()
+                    openDialog.value = false
+                }) { if (negativeText != null) Text(negativeText) }
+            },
+            title = { Text(title) },
+            text = {
+                if (text != null)
+                    Text(text)
+            }
+        )
+    }
+}
