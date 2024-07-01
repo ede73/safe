78b20c95963df1c3f8eb5f914928d84d47edb427
ede73 <ede@iki.fi>
2024-07-01 16:00:52 -0700

    sort prefs keys

--- app/src/main/java/fi/iki/ede/safe/model/Preferences.kt
+++ app/src/main/java/fi/iki/ede/safe/model/Preferences.kt
@@ -39,29 +39,29 @@ object Preferences {
         "Mustn't be used, no point for now, see ExportConfig/SUPPORT_EXPORT_LOCATION_MEMORY",
         level = DeprecationLevel.WARNING
     )
+    const val PREFERENCE_AUTOBACKUP_QUOTA_EXCEEDED = "autobackup_quota_exceeded"
+    const val PREFERENCE_AUTOBACKUP_RESTORE_FINISHED = "autobackup_restore_finished"
+    const val PREFERENCE_AUTOBACKUP_RESTORE_STARTED = "autobackup_restore_started"
+    const val PREFERENCE_AUTOBACKUP_STARTED = "autobackup_started"
     const val PREFERENCE_BACKUP_DOCUMENT = "backup_document"
-    const val PREFERENCE_EXPERIMENTAL_FEATURES = "experiments"
-    const val PREFERENCE_MAKE_CRASH = "make_a_crash"
     private val PREFERENCE_BACKUP_PATH_DEFAULT_VALUE =
         (Environment.getExternalStorageDirectory()?.absolutePath
             ?: "") + "/${PASSWORDSAFE_EXPORT_FILE}"
     const val PREFERENCE_BIOMETRICS_ENABLED = "biometrics"
-    const val PREFERENCE_LOCK_TIMEOUT_MINUTES = "lock_timeout"
-    private const val PREFERENCE_CLIPBOARD_CLEAR_DELAY = "clipboard_clear_delay"
-    private const val PREFERENCE_DEFAULT_USER_NAME = "default_user_name"
-    private const val NOTIFICATION_PERMISSION_REQUIRED = "notification_permission_required"
-    private const val NOTIFICATION_PERMISSION_DENIED = "notification_permission_denied"
     const val PREFERENCE_BIO_CIPHER = "bio_cipher"
+    const val PREFERENCE_EXPERIMENTAL_FEATURES = "experiments"
+    const val PREFERENCE_LAST_BACKUP_TIME = "time_of_last_backup"
+    const val PREFERENCE_LOCK_TIMEOUT_MINUTES = "lock_timeout"
+    const val PREFERENCE_MAKE_CRASH = "make_a_crash"
+    const val PREFERENCE_SOFT_DELETE_DAYS = "soft_delete_days"
+    private const val NOTIFICATION_PERMISSION_DENIED = "notification_permission_denied"
+    private const val NOTIFICATION_PERMISSION_REQUIRED = "notification_permission_required"
+    private const val PREFERENCE_CLIPBOARD_CLEAR_DELAY = "clipboard_clear_delay"
+    private const val PREFERENCE_CLIPBOARD_CLEAR_DELAY_DEFAULT_VALUE = "45"
+    private const val PREFERENCE_DEFAULT_USER_NAME = "default_user_name"
+    private const val PREFERENCE_GPM_IMPORT_USAGE_SHOWN = "gpm_import_usage_shown"
     private const val PREFERENCE_LOCK_ON_SCREEN_LOCK = "lock_on_screen_lock"
     private const val PREFERENCE_LOCK_TIMEOUT_DEFAULT_VALUE_MINUTES = "5"
-    private const val PREFERENCE_CLIPBOARD_CLEAR_DELAY_DEFAULT_VALUE = "45"
-    const val PREFERENCE_LAST_BACKUP_TIME = "time_of_last_backup"
-    const val PREFERENCE_AUTOBACKUP_QUOTA_EXCEEDED = "autobackup_quota_exceeded"
-    const val PREFERENCE_AUTOBACKUP_STARTED = "autobackup_started"
-    const val PREFERENCE_AUTOBACKUP_RESTORE_STARTED = "autobackup_restore_started"
-    const val PREFERENCE_AUTOBACKUP_RESTORE_FINISHED = "autobackup_restore_finished"
-    const val PREFERENCE_SOFT_DELETE_DAYS = "soft_delete_days"
-    private const val PREFERENCE_GPM_IMPORT_USAGE_SHOWN = "gpm_import_usage_shown"
 
     fun getBackupDocument() = if (SUPPORT_EXPORT_LOCATION_MEMORY) {
         sharedPreferences
