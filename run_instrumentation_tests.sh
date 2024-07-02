7ec56cbd282ea8137eff74b05338829a63b5cace
ede73 <ede@iki.fi>
2024-07-02 16:18:17 -0700

    CI pipeline for WSL box

---
+++ run_instrumentation_tests.sh
@@ -0,0 +1,40 @@
+REVPARSE=.last_rev_parse
+
+# Get the current commit hash
+before_pull=$(cat $REVPARSE)
+
+# Perform git pull
+git pull
+
+# Get the new commit hash
+after_pull=$(git rev-parse HEAD)
+
+[[ "$before_pull" == "$after_pull" ]] && {
+  exit 0
+}
+
+adb devices
+
+${ANDROID_HOME}/emulator/emulator -avd Pixel_2_API_33.avd -no-audio&
+emulatorpid=$!
+
+sleep 1
+emulator_serial=$(adb devices | grep emulator | cut -f1)
+
+boot_completed=""
+while [ -z "$boot_completed" ]; do
+    boot_completed=$(adb -s $emulator_serial shell getprop sys.boot_completed 2>&1 | tr -d '\r')
+    if [ "$boot_completed" != "1" ]; then
+        echo "Waiting for emulator to boot..."
+        sleep 5
+    fi
+done
+git pull
+[[ $(./gradlew connectedAndroidTest 2> >(sed 's,file:///,file://wsl.localhost/Ubuntu/,g' 1>&2)) ]] || {
+  echo $after_pull >$REVPARSE
+}
+
+adb -s $emulator_serial emu kill
+
+wait $emulatorpid
+
