56e115ec971ae447a4eccae047b5be1af3652cbc
ede73 <ede@iki.fi>
2024-07-03 09:19:05 -0700

    reformat

--- run_instrumentation_tests.sh
+++ run_instrumentation_tests.sh
@@ -1,47 +1,48 @@
 ci() {
-REVPARSE=.last_rev_parse
+  REVPARSE=.last_rev_parse
 
-# Get the current commit hash
-before_pull=$(cat $REVPARSE)
+  # Get the current commit hash
+  before_pull=$(cat $REVPARSE)
 
-# Perform git pull
-git pull
+  # Perform git pull
+  git pull
 
-# Get the new commit hash
-after_pull=$(git rev-parse HEAD)
+  # Get the new commit hash
+  after_pull=$(git rev-parse HEAD)
 
-[[ "$before_pull" == "$after_pull" ]] && {
-  return 0
-}
+  [[ "$before_pull" == "$after_pull" ]] && {
+    return 0
+  }
 
-adb devices
+  adb devices
 
-${ANDROID_HOME}/emulator/emulator -avd Pixel_2_API_33.avd -no-audio&
-emulatorpid=$!
+  "${ANDROID_HOME}/emulator/emulator" -avd Pixel_2_API_33.avd -no-audio&
+  emulatorpid=$!
 
-sleep 1
-emulator_serial=$(adb devices | grep emulator | cut -f1)
+  sleep 1
+  emulator_serial=$(adb devices | grep emulator | cut -f1)
 
-boot_completed=""
-while [ -z "$boot_completed" ]; do
-    boot_completed=$(adb -s $emulator_serial shell getprop sys.boot_completed 2>&1 | tr -d '\r')
-    if [ "$boot_completed" != "1" ]; then
-        echo "Waiting for emulator to boot..."
-        sleep 5
-    fi
-done
+  boot_completed=""
+  while [ -z "$boot_completed" ]; do
+      boot_completed=$(adb -s "$emulator_serial" shell getprop sys.boot_completed 2>&1 | tr -d '\r')
+      if [ "$boot_completed" != "1" ]; then
+          echo "Waiting for emulator to boot..."
+          sleep 5
+      fi
+  done
 
-touch .failed
-[[ $(./gradlew connectedAndroidTest 2> >(sed 's,file:///,file://wsl.localhost/Ubuntu/,g' 1>&2)) ]] || {
-  echo $after_pull >$REVPARSE
-  rm .failed
-}
+  touch .failed
+  [[ $(./gradlew connectedAndroidTest 2> >(sed 's,file:///,file://wsl.localhost/Ubuntu/,g' 1>&2)) ]] || {
+    echo "Tests run successfully"
+    echo "$after_pull" >$REVPARSE
+    rm .failed
+  }
 
-adb -s $emulator_serial emu kill
+  ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=5 adb -s "$emulator_serial" emu kill
+  kill $emulatorpid
+  wait $emulatorpid
 
-wait $emulatorpid
-
-[[ -f .failed ]] && return 10
+  [[ -f .failed ]] && return 10
 }
 
 [[ -f .failed ]] && {
