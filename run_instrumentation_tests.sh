6aaf8b2b12c27cd296c6d2a179958cb9d581a3d5
ede73 <ede@iki.fi>
2024-07-03 09:40:16 -0700

    enhancements

--- run_instrumentation_tests.sh
+++ run_instrumentation_tests.sh
@@ -1,6 +1,6 @@
-ci() {
-  REVPARSE=.last_rev_parse
+REVPARSE=.last_rev_parse
 
+have_new_commits() {
   # Get the current commit hash
   before_pull=$(cat $REVPARSE)
 
@@ -11,36 +11,64 @@ ci() {
   after_pull=$(git rev-parse HEAD)
 
   [[ "$before_pull" == "$after_pull" ]] && {
-    return 0
+    return 1
   }
+  return 0
+}
 
+start_emulator() {
   adb devices
+  "${ANDROID_HOME}/emulator/emulator" -avd Pixel_2_API_33.avd -no-audio >/dev/null 2>&1
+}
 
-  "${ANDROID_HOME}/emulator/emulator" -avd Pixel_2_API_33.avd -no-audio&
-  emulatorpid=$!
-
+get_emu_serial() {
   sleep 1
-  emulator_serial=$(adb devices | grep emulator | cut -f1)
+  adb devices | grep emulator | cut -f1
+}
+
+wait_emu_online() {
+  emulator_serial="$1"
 
   boot_completed=""
   while [ -z "$boot_completed" ]; do
       boot_completed=$(adb -s "$emulator_serial" shell getprop sys.boot_completed 2>&1 | tr -d '\r')
       if [ "$boot_completed" != "1" ]; then
           echo "Waiting for emulator to boot..."
-          sleep 2
+          sleep 1
+          boot_completed=""
       fi
   done
+  sleep 1
+}
+
+kill_emu() {
+  emulator_serial="$1"
+  emulatorpid="$2"
+  ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=5 adb -s "$emulator_serial" emu kill
+  kill $emulatorpid
+  wait $emulatorpid
+}
+
+# return 0 if you want while to continue
+ci() {
+  have_new_commits || {
+    return 0
+  }
+
+  start_emulator&
+  emulatorpid=$!
+
+  emulator_serial=$(get_emu_serial)
+  time wait_emu_online "$emulator_serial"
 
   touch .failed
-  [[ $(./gradlew connectedAndroidTest 2> >(sed 's,file:///,file://wsl.localhost/Ubuntu/,g' 1>&2)) ]] || {
+  [[ $(./gradlew -q connectedAndroidTest 2> >(sed 's,file:///,file://wsl.localhost/Ubuntu/,g' 1>&2)) ]] || {
     echo "Tests run successfully"
     echo "$after_pull" >$REVPARSE
     rm .failed
   }
 
-  ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=5 adb -s "$emulator_serial" emu kill
-  kill $emulatorpid
-  wait $emulatorpid
+  kill_emu "$emulator_serial" "$emulatorpid"
 
   [[ -f .failed ]] && return 10
 }
