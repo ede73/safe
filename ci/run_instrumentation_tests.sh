da5c2d0ed32606504f0f9e68fcc4a1317a66fb0b
ede73 <ede@iki.fi>
2024-07-03 11:05:56 -0700

    isolate adb

--- ci/run_instrumentation_tests.sh
+++ ci/run_instrumentation_tests.sh
@@ -2,6 +2,12 @@ REVPARSE=.last_rev_parse
 # Trying to ensure WSL adb doesn't clash with windows one...
 export ADB_SERVER_SOCKET=tcp:localhost:6600
 
+run_adb_command() {
+  ADB_SERVER_SOCKET=tcp:localhost:6600 $*
+}
+
+run_adb_command start-server
+
 get_emu_port() {
   echo "6700"
 }
@@ -24,13 +30,13 @@ have_new_commits() {
 }
 
 start_emulator() {
-  adb devices
+  run_adb_command devices
   "${ANDROID_HOME}/emulator/emulator" -avd Pixel_2_API_33.avd -no-audio -port "$(get_emu_port)" >/dev/null 2>&1
 }
 
 get_emu_serial() {
   sleep 1
-  adb devices | grep emulator | cut -f1
+  run_adb_command adb devices | grep emulator | cut -f1
 }
 
 wait_emu_online() {
@@ -38,7 +44,7 @@ wait_emu_online() {
 
   boot_completed=""
   while [ -z "$boot_completed" ]; do
-      boot_completed=$(adb -s "$emulator_serial" shell getprop sys.boot_completed 2>&1 | tr -d '\r')
+      boot_completed=$(run_adb_command -s "$emulator_serial" shell getprop sys.boot_completed 2>&1 | tr -d '\r')
       if [ "$boot_completed" != "1" ]; then
           echo "Waiting for emulator to boot..."
           sleep 1
@@ -51,7 +57,7 @@ wait_emu_online() {
 kill_emu() {
   emulator_serial="$1"
   emulatorpid="$2"
-  ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=5 adb -s "$emulator_serial" emu kill
+  ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=5 run_adb_command -s "$emulator_serial" emu kill
   kill $emulatorpid
   wait $emulatorpid
 }
