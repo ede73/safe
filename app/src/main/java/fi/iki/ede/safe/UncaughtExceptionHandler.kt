8a797b7fd5a1184cd7ace728ffc263c03e802203
ede73 <ede@iki.fi>
2024-07-08 10:52:09 -0700

    in debug mode the uncaught exeption handler/firebase doesnt log properly

--- app/src/main/java/fi/iki/ede/safe/UncaughtExceptionHandler.kt
+++ app/src/main/java/fi/iki/ede/safe/UncaughtExceptionHandler.kt
@@ -1,6 +1,9 @@
 package fi.iki.ede.safe
 
+import android.util.Log
 import fi.iki.ede.safe.model.Preferences
+
+private val TAG = "MyExceptionHandler"
 
 class MyExceptionHandler(private val defaultHandler: Thread.UncaughtExceptionHandler) :
     Thread.UncaughtExceptionHandler {
@@ -9,6 +12,9 @@ class MyExceptionHandler(private val defaultHandler: Thread.UncaughtExceptionHan
         // Log the exception to Crashlytics - shouldn't be needed though
         // FirebaseCrashlytics.getInstance().recordException(exception)
 
+        if (BuildConfig.DEBUG) {
+            Log.e(TAG, "Uncaught exception", exception)
+        }
         // Clear all plugins
         try {
             Preferences.clearAllPlugins()
