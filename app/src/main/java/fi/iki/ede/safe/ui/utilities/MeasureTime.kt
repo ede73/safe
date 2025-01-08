8f4876e834234bfebb457c2759248c3b9e5fe6cc
ede73 <ede@iki.fi>
2025-01-07 19:27:56 -0800

    Add autoclose and automatic time exceeed warning system

--- app/src/main/java/fi/iki/ede/safe/ui/utilities/MeasureTime.kt
+++ app/src/main/java/fi/iki/ede/safe/ui/utilities/MeasureTime.kt
@@ -2,31 +2,53 @@ package fi.iki.ede.safe.ui.utilities
 
 import android.util.Log
 import fi.iki.ede.safe.BuildConfig
+import kotlin.time.Duration
 
-class MeasureTime(private val title: String = "") {
-    private val TAG = "MeasureTime"
+class MeasureTime(private val title: String = "") : AutoCloseable {
+    private val TAG = "MT"
     private var startTime = System.nanoTime()
-    private var lastLapTime = System.nanoTime()
+    private var lastLapTime = startTime
+    private var ended = false
 
     fun start() {
         if (!BuildConfig.DEBUG) return
         val now = System.nanoTime()
         startTime = now
         lastLapTime = now
+        Log.i(makeTag(), "========== START ==========")
     }
 
-    fun lap(message: String) {
+    fun lap(message: String, maxDuration: Duration? = null) {
         if (!BuildConfig.DEBUG) return
         val now = System.nanoTime()
         val sinceStart = now - startTime
         val sinceLastLap = now - lastLapTime
-        Log.e(
-            "$TAG${if (title.isEmpty()) "" else " - $title"}",
+
+        val msg =
             "It took ${formatTime(sinceStart)} since start / ${formatTime(sinceLastLap)} to perform $message"
-        )
+
+        if (maxDuration != null && maxDuration.inWholeNanoseconds < sinceLastLap) {
+            // shit, we exceeded allotted time
+            Log.e(makeTag(), "EXCEEDED ${formatTime(maxDuration.inWholeNanoseconds)} $msg")
+        } else {
+            Log.i(makeTag(), msg)
+        }
         lastLapTime = now
     }
 
+    fun end(message: String, maxDuration: Duration? = null) {
+        lap(message, maxDuration)
+        Log.i(makeTag(), "========== STOP ==========")
+        ended = true
+    }
+
+    override fun close() {
+        if (!ended) {
+            end("You forgot to call .end()")
+        }
+    }
+
+    private fun makeTag() = "$TAG${if (title.isEmpty()) "" else "-${title}"}"
     private fun formatTime(time: Long): String {
         return when {
             time < 1_000 -> "$time ns"
